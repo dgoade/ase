dca56f2dac3b91f606b081fbdd05be39665fd804
dgoade <dgoade@gmail.com>
2015-05-03 17:17:28 -0400

    Added ase_output_logging_2.py -- logging using configDict and PyYAML (python2.7)

--- python/ase_exec.py
+++ python/ase_exec.py
@@ -1,32 +1,49 @@
 #!/usr/bin/env python
 
-import logging 
-import logging.config
+"""
+Automation Scripting Essential Skills (https://github.com/dgoade/ase)
+Python
+
+Execution: Execute an external command or script
+
+Technique1 -- Use python2.7's core logging and configDict with yaml,
+and fabric to execute an external command or shell script and
+capture stdout and stderr separately
+
+"""
+
+import sys
+import yaml
+import logging
+import re
+from logging.config import dictConfig
+from pprint import pformat
 from fabric.api import hide,local,env,settings,warn_only
 
-LogFileName='./ase_exec.log'
-LogLevel='DEBUG'
-Verbose=True
-
-def exec_script(script_name, script_opts):
-
-    '''
-    a good template
-    function for general-purpose automation tasks
-    that require executing an external shell script
-    using fabric and capturing the stdout and stderr
-    while keeping them separate.
-    '''
+def execute(script_name, script_opts):
 
     rval = 0
-    logger = logging.getLogger('{0}.exec_script'.format(__name__))
+    f = open(sys.argv[1])
+    config_dict = yaml.safe_load(f)
+    f.close()
+
+    if config_dict:
+        dictConfig(config_dict['logging'])
+    else:
+        pass
+
+    logger = logging.getLogger('execute.{0}'.format(__name__))
+    log_msg = 'Logging config dictionary loaded successfully.'
+    logger.debug(log_msg)
+    logger.debug('Logging pformatted data of the config_dict')
+    logger.debug(pformat(config_dict))
 
     com='{0} "{1}"'.format(
         script_name,
         script_opts)
 
-    logger = logging.getLogger('{0}.exec_script'.format(__name__))
-    log_msg = 'Running external shell script: {0}'.format(script_name)
+    log_msg = 'Running external command / script: {0}'.format(script_name)
+    logger.debug(log_msg)
 
     try:
         with hide('everything'):
@@ -34,39 +51,39 @@ def exec_script(script_name, script_opts):
             logger.debug('return_code={0}'.format(proc.return_code))
 
             if proc.return_code == 0:
-                log_msg = 'Script executed successfully: {0}'
+                log_msg = 'Executed successfully: {0}'
                 log_msg = log_msg.format(com)
                 logger.debug(log_msg)
                 rval = proc.return_code
             else:
-                log_msg = 'Script execution failed: {0}'
-                log_msg = log_msg = format(com)
+                log_msg = 'Execution failed: {0}'
+                log_msg = log_msg.format(com)
                 logger.error(log_msg)
 
         rval = proc.return_code
 
     except Exception:
-        log_msg = "Exception occurred execute script"
+        log_msg = "Exception occurred during execution"
         logger.exception(log_msg)
         rval = 1
 
     if( rval == 0 ):
         if( logger.isEnabledFor('DEBUG') ):
             if( proc.stdout ):
-                log_msg = "--stdout from external script follows:\n{0}"
+                log_msg = "--stdout from execution follows:\n{0}"
                 log_msg = log_msg.format(proc.stdout)
                 logger.debug(log_msg)
             if( proc.stderr ):
-                log_msg = "--stderr from external script follows:\n{0}"
+                log_msg = "--stderr from execution follows:\n{0}"
                 log_msg = log_msg.format(proc.stderr)
                 logger.debug(log_msg)
     else:
         if( proc.stdout ):
-            log_msg = "--stdout from external script follows:\n{0}"
+            log_msg = "--stdout from execution follows:\n{0}"
             log_msg = log_msg.format(proc.stdout)
             logger.error(log_msg)
         if( proc.stderr ):
-            log_msg = "--stderr from external script follows:\n{0}"
+            log_msg = "--stderr from execution follows:\n{0}"
             log_msg = log_msg.format(proc.stderr)
             logger.error(log_msg)
 
@@ -74,17 +91,6 @@ def exec_script(script_name, script_opts):
 
 def main():
 
-    logging.config.fileConfig('conf/ase_logging.conf')
-    logger = logging.getLogger('root')
-
-    if Verbose:
-        # Add the console handler
-        ch = logging.StreamHandler()
-        ch.setLevel(1)
-        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
-
-    exec_script('ls', '-l')    
+    execute('ls', '-l')
 
 if __name__ == "__main__": main()
